519d0c8f9172d09f0937364713257b185a6f8361
liangHenry <cczhaoliang@126.com>
2017-05-30 16:16:54 +0800

    修改主页样式

--- src/main/java/com/readmunity/service/impl/WelcomeServiceImpl.java
+++ src/main/java/com/readmunity/service/impl/WelcomeServiceImpl.java
@@ -33,7 +33,7 @@ public class WelcomeServiceImpl implements WelcomeService{
     public List<Book> getRankingBook(){
         List<Book> arrayList=new ArrayList<Book>(10);
         for(int i=0;i<10;i++) {
-            Book book1 = new Book("我的抉择时刻"+i, "希拉里的最棒的回忆。");
+            Book book1 = new Book("我的抉择时刻"+i, i+"希拉里·克林顿权威传记，讲透其人生的每一个关键点与抉择时刻，洞悉在美国政坛不断缔造奇迹，永不止步的力量源泉。");
             arrayList.add(book1);
         }
         return arrayList;
