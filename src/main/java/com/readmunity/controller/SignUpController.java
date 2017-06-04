b510f8e5f25f510fe7c77c91037c652c4e226b6a
liangHenry <cczhaoliang@126.com>
2017-06-05 01:41:47 +0800

    增加登录验证，简单写法 spring security 4.2

--- src/main/java/com/readmunity/controller/SignUpController.java
+++ src/main/java/com/readmunity/controller/SignUpController.java
@@ -2,6 +2,7 @@ package com.readmunity.controller;
 
 import com.readmunity.service.impl.BookServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +15,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class SignUpController {
 
     @RequestMapping(value = "signUp", method = RequestMethod.GET)
-    public String getList(ModelMap map) {
+    public String getSignUp(ModelMap map) {
         return "signUp";
+    }
+
+    @RequestMapping(value = "signIn", method = RequestMethod.GET)
+    public String getSignIn(ModelMap map) {
+        return "signIn";
+    }
+    @RequestMapping("/signIn-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "signIn";
     }
 
 }
---
+++ src/main/java/com/readmunity/security/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.readmunity.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/public/error/*","/error/*").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/bootstrap/**").permitAll()
+                .antMatchers("/markdown/**").permitAll()
+                .antMatchers("/**/*.js").permitAll()
+                .antMatchers("/**/*.css").permitAll()
+                .antMatchers("/sign/signIn","/sign/signUp","/sign/signIn-error").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/sign/signIn").failureUrl("/sign/signIn-error")
+                .defaultSuccessUrl("/user").permitAll()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("123456").roles("USER").and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN");;
+    }
+
+}
